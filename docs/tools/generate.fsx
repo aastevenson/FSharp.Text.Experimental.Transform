b287d439208f0d5567be6118f7a6abc5c7482e50
Andrew Stevenson <andrew.a.stevenson@rogers.com>
2016-05-17 21:59:16 -0400

    Update webpage link from local testing to online testing

--- docs/tools/generate.fsx
+++ docs/tools/generate.fsx
@@ -11,7 +11,7 @@
 // (This is the original behaviour of ProjectScaffold prior to multi project support)
 let referenceBinaries = []
 // Web site location for the generated documentation
-let website = "" // empty for local testing. Replace with "/FSharp.Text.Experimental.Transform"
+let website = "/FSharp.Text.Experimental.Transform"
 
 let githubLink = "http://github.com/aastevenson/FSharp.Text.Experimental.Transform"
 
